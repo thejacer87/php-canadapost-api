bd376fbef8253321cd3bbe1ea874d27b13f3b3cf
jbennest <jbennest@acromedia.com>
2018-07-11 15:30:17 -0700

    add first test

---
+++ tests/src/CanadaPost/Tests/RateTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CanadaPost\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Rate Class Tests.
+ *
+ * @group Rate
+ */
+class RateTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+
+    }
+
+    public function testGetRate()
+    {
+      $this->assertTrue(TRUE);
+    }
+}
