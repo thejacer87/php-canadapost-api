d3ada4ef46651095b5bb765e82744d4c1c68b770
Jace Bennest <jacebennest87@gmail.com>
2018-07-17 09:07:16 -0700

    tests all rates

--- tests/CanadaPost/Tests/RatingTest.php
+++ tests/CanadaPost/Tests/RatingTest.php
@@ -48,15 +48,41 @@ class RatingTest extends PHPUnit_Framework_TestCase
         ]);
         $handler = HandlerStack::create($mock);
         $response = $this->ratingService->getRates('', '', 1, ['handler' => $handler]);
-        foreach ($response['price-quotes'] as $quotes) {
-            $this->assertTrue(is_array($quotes));
-            foreach ($quotes as $rate) {
-                $this->assertTrue(is_array($rate));
-                $this->assertNotEmpty($rate['service-code']);
-                $this->assertNotEmpty($rate['service-name']);
-                $this->assertGreaterThan(0, $rate['price-details']['base']);
-            }
-        }
+
+        // Check response.
+        $this->assertTrue(is_array($response['price-quotes']));
+
+        // Test the rates.
+        $rates = $response['price-quotes']['price-quote'];
+        $this->assertTrue(is_array($rates));
+
+        // Expedited Parcel.
+        $this->assertTrue(is_array($rates[0]));
+        $this->assertEquals('DOM.EP', $rates[0]['service-code']);
+        $this->assertEquals('Expedited Parcel', $rates[0]['service-name']);
+        $this->assertTrue(is_array($rates[0]['price-details']));
+        $this->assertEquals('9.59', $rates[0]['price-details']['base']);
+
+        // Priority Courier.
+        $this->assertTrue(is_array($rates[1]));
+        $this->assertEquals('DOM.PC', $rates[1]['service-code']);
+        $this->assertEquals('Priority Courier', $rates[1]['service-name']);
+        $this->assertTrue(is_array($rates[1]['price-details']));
+        $this->assertEquals('22.64', $rates[1]['price-details']['base']);
+
+        // Priority Courier.
+        $this->assertTrue(is_array($rates[2]));
+        $this->assertEquals('DOM.RP', $rates[2]['service-code']);
+        $this->assertEquals('Regular Parcel', $rates[2]['service-name']);
+        $this->assertTrue(is_array($rates[2]['price-details']));
+        $this->assertEquals('9.59', $rates[2]['price-details']['base']);
+
+        // Priority Courier.
+        $this->assertTrue(is_array($rates[3]));
+        $this->assertEquals('DOM.XP', $rates[3]['service-code']);
+        $this->assertEquals('Xpresspost', $rates[3]['service-name']);
+        $this->assertTrue(is_array($rates[3]['price-details']));
+        $this->assertEquals('12.26', $rates[3]['price-details']['base']);
     }
 
 }
