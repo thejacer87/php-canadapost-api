934930e4b264546192d64d6cc950684f93b8f995
Jace Bennest <jbennest@acromedia.com>
2018-08-14 14:16:17 -0700

    parse options codes

--- src/Rating.php
+++ src/Rating.php
@@ -6,6 +6,21 @@ use LSS\Array2XML;
 
 class Rating extends ClientBase
 {
+
+    /**
+     * The Canada Post-specific option codes,
+     *
+     * @see https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/rating/getrates/default.jsf
+     */
+    public static $VALID_OPTION_CODES = [
+      'SO',
+      'PA18',
+      'PA19',
+      'HFP',
+      'DNS',
+      'LAD',
+    ];
+
     /**
      * Get the shipping rates for the given locations and weight.
      *
@@ -37,6 +52,9 @@ class Rating extends ClientBase
                     'postal-code' => $postalCode,
                 ],
             ],
+            'options' => [
+              'option' => $this->getOptionCodes($options),
+            ],
         ];
         $xml = Array2XML::createXML('mailing-scenario', $content);
         $envelope = $xml->documentElement;
@@ -53,5 +71,39 @@ class Rating extends ClientBase
             $options
         );
         return $response;
+    }
+    
+    /**
+     * Helper function to extract the option codes.
+     *
+     * @param array $options
+     *  The options array.
+     *
+     * @return array
+     *  The list of options with the option-code.
+     */
+    private function getOptionCodes(array $options) {
+      if (!$options['option_codes']) {
+        return [];
+      }
+
+      $valid_options= [];
+      foreach ($options['option_codes'] as $optionCode) {
+        if (!in_array(strtoupper($optionCode), $this::$VALID_OPTION_CODES)) {
+          break;
+        }
+        // @todo Perhaps we should check for conflicts here, might be overkill.
+        // From Canada Post docs:
+        // There are some options that can be applied to a shipment that
+        // conflict with the presence of another option. You can use the
+        // "Get Option" call in advance to check the contents of the
+        // <conflicting-options> group from a Get Option call for options
+        // selected by end users or options available for a given service.
+        $valid_options[] = [
+          'option-code' => $optionCode
+        ];
+      }
+
+      return $valid_options;
     }
 }
