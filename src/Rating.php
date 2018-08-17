8901d08264be1d47bc24a5c43647dc1d199f013e
thejacer87 <jacebennest87@gmail.com>
2018-08-17 11:59:35 -0700

    Add support for Canada Post option codes (#11)

--- src/Rating.php
+++ src/Rating.php
@@ -38,6 +38,11 @@ class Rating extends ClientBase
                 ],
             ],
         ];
+
+        if (!empty($options['option_codes'])) {
+            $content['options']['option'] = $this->parseOptionCodes($options);
+        }
+
         $xml = Array2XML::createXML('mailing-scenario', $content);
         $envelope = $xml->documentElement;
         $envelope->setAttribute('xmlns', 'http://www.canadapost.ca/ws/ship/rate-v3');
@@ -53,5 +58,55 @@ class Rating extends ClientBase
             $options
         );
         return $response;
+    }
+    
+    /**
+     * Get the Canada Post-specific option codes,
+     *
+     * @return array
+     *   The array of option codes.
+     *
+     * @see https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/rating/getrates/default.jsf
+     */
+    public static function getOptionCodes()
+    {
+        return [
+          'SO'   => 'Signature (SO)',
+          'PA18' => 'Proof of Age Required - 18 (PA18)',
+          'PA19' => 'Proof of Age Required - 19 (PA19)',
+          'HFP'  => 'Card for pickup (HFP)',
+          'DNS'  => 'Do not safe drop (DNS)',
+          'LAD'  => 'Leave at door - do not card (LAD)',
+        ];
+    }
+    
+    /**
+     * Helper function to extract the option codes.
+     *
+     * @param array $options
+     *  The options array.
+     *
+     * @return array
+     *  The list of options with the option-code.
+     */
+    protected function parseOptionCodes(array $options) {
+      $valid_options= [];
+      foreach ($options['option_codes'] as $optionCode) {
+        if (!in_array(strtoupper($optionCode), self::getOptionCodes())) {
+          continue;
+        }
+        // @todo Perhaps we should check for conflicts here, might be overkill.
+        // From Canada Post docs:
+        // There are some options that can be applied to a shipment that
+        // conflict with the presence of another option. You can use the
+        // "Get Option" call in advance to check the contents of the
+        // <conflicting-options> group from a Get Option call for options
+        // selected by end users or options available for a given service.
+        $valid_options[] = [
+          'option-code' => $optionCode
+        ];
+      }
+
+      return $valid_options;
     }
 }
