27ba7a24b680ffc93c178be9f68f1030f2462a25
thejacer87 <jacebennest87@gmail.com>
2018-10-10 08:43:01 -0700

    Shipment and NCShipment api (#12)
    add shipment and ncshipment api

--- src/ClientBase.php
+++ src/ClientBase.php
@@ -3,6 +3,7 @@
 namespace CanadaPost;
 
 use CanadaPost\Exception\ClientException;
+use DateTime;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\ClientException as GuzzleClientException;
 use GuzzleHttp\Psr7\Response;
@@ -61,6 +62,13 @@ abstract class ClientBase
     protected $customerNumber;
 
     /**
+     * The Canada Post API contract ID.
+     *
+     * @var array
+     */
+    protected $contractId;
+
+    /**
      * ClientBase constructor.
      *
      * @param array $config
@@ -91,7 +99,7 @@ abstract class ClientBase
      *     - handler: Don't use unless you have a valid reason or for unit
      *       testing - http://docs.guzzlephp.org/en/stable/testing.html#mock-handler
      *
-     * @return \DOMDocument
+     * @return \DOMDocument|\Psr\Http\Message\StreamInterface
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function get($endpoint, array $headers = [], array $options = [])
@@ -119,7 +127,11 @@ abstract class ClientBase
             );
         }
 
-        return $this->parseResponse($response);
+        if (empty($options['raw_response'])) {
+            return $this->parseResponse($response);
+        }
+
+        return $response->getBody();
     }
 
     /**
@@ -141,8 +153,12 @@ abstract class ClientBase
      * @return \DOMDocument
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function post($endpoint, array $headers = [], $payload, array $options = [])
-    {
+    public function post(
+        $endpoint,
+        array $headers = [],
+        $payload,
+        array $options = []
+    ) {
         $url = $this->baseUrl . '/' . $endpoint;
 
         try {
@@ -171,6 +187,99 @@ abstract class ClientBase
     }
 
     /**
+     * Send the DELETE request to the Canada Post API.
+     *
+     * @param string $endpoint
+     *   The endpoint to send the request.
+     * @param array $headers
+     *   The HTTP headers array.
+     * @param array $options
+     *   An array of options. Supported options are all request options
+     * supported by Guzzle http://docs.guzzlephp.org/en/stable/request-options.html
+     * plus the following:
+     *     - handler: Don't use unless you have a valid reason or for unit
+     *       testing - http://docs.guzzlephp.org/en/stable/testing.html#mock-handler
+     *
+     * @return \DOMDocument|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function delete($endpoint, array $headers = [], array $options = [])
+    {
+        $url = $this->baseUrl . '/' . $endpoint;
+
+        try {
+            $client = $this->buildClient($options);
+            $options += [
+                'auth' => [$this->username, $this->password],
+                'headers' => $headers,
+            ];
+
+            $response = $client->request('DELETE', $url, $options);
+        } catch (GuzzleClientException $exception) {
+            $response = $exception->getResponse();
+
+            throw new ClientException(
+                $exception->getMessage(),
+                $this->parseResponse($response),
+                $exception->getRequest(),
+                $response,
+                $exception->getPrevious(),
+                $exception->getHandlerContext()
+            );
+        }
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * Send the GET request to the Canada Post API for a specific file.
+     *
+     * @param string $endpoint
+     *   The endpoint to send the request.
+     * @param string $fileType
+     *   The file type of the file to retrieve.
+     * @param array $options
+     *   An array of options. Supported options are all request options
+     * supported by Guzzle http://docs.guzzlephp.org/en/stable/request-options.html
+     * plus the following:
+     *     - handler: Don't use unless you have a valid reason or for unit
+     *       testing - http://docs.guzzlephp.org/en/stable/testing.html#mock-handler
+     *
+     * @return \DOMDocument|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getFile($endpoint, $fileType, array $options = [])
+    {
+        return $this->get(
+            $endpoint,
+            [
+                'Accept' => 'application/' . $fileType,
+                'Accept-Language' => 'en-CA',
+            ],
+            $options
+        );
+    }
+
+    /**
+     * Get an artifact from Canada Post server.
+     * @param string $endpoint
+     *   The endpoint of the file to retrieve from the Canada Post server.
+     * @param array $options
+     *   The options array.
+     *
+     * @return \DOMDocument|\Psr\Http\Message\StreamInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getArtifact($endpoint, array $options = [])
+    {
+        $response = $this->getFile(
+            $endpoint,
+            'pdf',
+            array_merge($options, ['raw_response' => true])
+        );
+        return $response;
+    }
+
+    /**
      * Set the API configuration array for the Client.
      *
      * @param array $config
@@ -186,6 +295,7 @@ abstract class ClientBase
         $this->username = $config['username'];
         $this->password = $config['password'];
         $this->customerNumber = $config['customer_number'];
+        $this->contractId = isset($config['contract_id']) ? $config['contract_id'] : null;
     }
 
     /**
@@ -255,9 +365,10 @@ abstract class ClientBase
      *
      * @return \GuzzleHttp\Client
      */
-    protected function buildClient(array &$options) {
+    protected function buildClient(array &$options)
+    {
         if (!isset($options['debug']) && $this->config['env'] === self::ENV_DEVELOPMENT) {
-            $options['debug'] = TRUE;
+            $options['debug'] = true;
         }
 
         if (!isset($options['handler'])) {
@@ -267,5 +378,102 @@ abstract class ClientBase
         $client = new GuzzleClient(['handler' => $options['handler']]);
         unset($options['handler']);
         return $client;
+    }
+
+    /**
+     * Helper function to extract the option codes.
+     *
+     * @param array $options
+     *  The options array.
+     *
+     * @return array
+     *  The list of options with the option-code.
+     */
+    protected function parseOptionCodes(array $options)
+    {
+        $valid_options = [];
+        foreach ($options['option_codes'] as $optionCode) {
+            if (!in_array(strtoupper($optionCode), self::getOptionCodes())) {
+                $message = sprintf(
+                    'Unsupported option code "%s". Supported options codes are "%s"',
+                    $optionCode,
+                    implode(', ', array_keys(self::getOptionCodes()))
+                );
+                throw new \InvalidArgumentException($message);
+            }
+            // @todo Perhaps we should check for conflicts here, might be overkill.
+            // From Canada Post docs:
+            // There are some options that can be applied to a shipment that
+            // conflict with the presence of another option. You can use the
+            // "Get Option" call in advance to check the contents of the
+            // <conflicting-options> group from a Get Option call for options
+            // selected by end users or options available for a given service.
+            $valid_options[] = [
+                'option-code' => $optionCode,
+            ];
+        }
+
+        return $valid_options;
+    }
+
+    /**
+     * Get the Canada Post-specific option codes,
+     *
+     * @return array
+     *   The array of option codes.
+     *
+     * @see https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/rating/getrates/default.jsf
+     */
+    public static function getOptionCodes()
+    {
+        return [
+            'SO' => 'Signature (SO)',
+            'PA18' => 'Proof of Age Required - 18 (PA18)',
+            'PA19' => 'Proof of Age Required - 19 (PA19)',
+            'HFP' => 'Card for pickup (HFP)',
+            'DNS' => 'Do not safe drop (DNS)',
+            'LAD' => 'Leave at door - do not card (LAD)',
+        ];
+    }
+
+    /**
+     * Helper function to format the postal code for an address.
+     *
+     * Canada Post API requires no spaces and uppercase postal code.
+     *
+     * @param array $postal_code
+     *   The postal code to verify.
+     */
+    protected function formatPostalCode(&$postal_code)
+    {
+        strtoupper(str_replace(' ', '', $postal_code));
+    }
+
+    /**
+     * Helper function to verify the dates are valid.
+     *
+     * @param string $from
+     *   The beginning date.
+     * @param $to
+     *   The end date.
+     * @param string $format
+     *   The date format to verify.
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function verifyDates($from, $to, $format = 'YmdHs')
+    {
+        if (!DateTime::createFromFormat($format, $from)) {
+            $message = 'The $from date is improperly formatted. Please use "YmdHs".';
+            throw new \InvalidArgumentException($message);
+        }
+        if (!DateTime::createFromFormat($format, $to)) {
+            $message = 'The $to date is improperly formatted. Please use "YmdHs".';
+            throw new \InvalidArgumentException($message);
+        }
+        if (new DateTime($from) > new DateTime($to)) {
+            $message = 'The $from date cannot be a later date than the $to date.';
+            throw new \InvalidArgumentException($message);
+        }
     }
 }
--- src/Rating.php
+++ src/Rating.php
@@ -6,6 +6,7 @@ use LSS\Array2XML;
 
 class Rating extends ClientBase
 {
+
     /**
      * Get the shipping rates for the given locations and weight.
      *
@@ -19,6 +20,7 @@ class Rating extends ClientBase
      *   The options to pass along to the Guzzle Client.
      *
      * @return \DOMDocument
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getRates($originPostalCode, $postalCode, $weight, array $options = [])
     {
@@ -45,7 +47,10 @@ class Rating extends ClientBase
 
         $xml = Array2XML::createXML('mailing-scenario', $content);
         $envelope = $xml->documentElement;
-        $envelope->setAttribute('xmlns', 'http://www.canadapost.ca/ws/ship/rate-v3');
+        $envelope->setAttribute(
+            'xmlns',
+            'http://www.canadapost.ca/ws/ship/rate-v3'
+        );
         $payload = $xml->saveXML();
 
         $response = $this->post(
@@ -58,55 +63,5 @@ class Rating extends ClientBase
             $options
         );
         return $response;
-    }
-    
-    /**
-     * Get the Canada Post-specific option codes,
-     *
-     * @return array
-     *   The array of option codes.
-     *
-     * @see https://www.canadapost.ca/cpo/mc/business/productsservices/developers/services/rating/getrates/default.jsf
-     */
-    public static function getOptionCodes()
-    {
-        return [
-          'SO'   => 'Signature (SO)',
-          'PA18' => 'Proof of Age Required - 18 (PA18)',
-          'PA19' => 'Proof of Age Required - 19 (PA19)',
-          'HFP'  => 'Card for pickup (HFP)',
-          'DNS'  => 'Do not safe drop (DNS)',
-          'LAD'  => 'Leave at door - do not card (LAD)',
-        ];
-    }
-    
-    /**
-     * Helper function to extract the option codes.
-     *
-     * @param array $options
-     *  The options array.
-     *
-     * @return array
-     *  The list of options with the option-code.
-     */
-    protected function parseOptionCodes(array $options) {
-      $valid_options= [];
-      foreach ($options['option_codes'] as $optionCode) {
-        if (!in_array(strtoupper($optionCode), self::getOptionCodes())) {
-          continue;
-        }
-        // @todo Perhaps we should check for conflicts here, might be overkill.
-        // From Canada Post docs:
-        // There are some options that can be applied to a shipment that
-        // conflict with the presence of another option. You can use the
-        // "Get Option" call in advance to check the contents of the
-        // <conflicting-options> group from a Get Option call for options
-        // selected by end users or options available for a given service.
-        $valid_options[] = [
-          'option-code' => $optionCode
-        ];
-      }
-
-      return $valid_options;
     }
 }
